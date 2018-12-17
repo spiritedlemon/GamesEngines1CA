08100edce2a4ed608c39dd85fd9c35290e025877
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2018-12-17 15:02:24 +0000

    Trying different approach to procedural gen

---
+++ GamesEngines1CA/Assets/Scripts/TerrainGen2/HexMetrics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMetrics : MonoBehaviour {
+	
+	public const float outerRadius = 10f;
+
+	public const float innerRadius = outerRadius * 0.866025404f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(0f, 0f, -outerRadius),
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+	};
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
