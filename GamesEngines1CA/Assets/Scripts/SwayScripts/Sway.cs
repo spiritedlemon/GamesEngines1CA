0c9848c3f76b5570e92fd49a2931f6d62f4320a0
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2018-12-18 15:04:48 +0000

    Started work on sway scripts

---
+++ GamesEngines1CA/Assets/Scripts/SwayScripts/Sway.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sway : MonoBehaviour {
+    public float angle = 20.0f;
+    public float frequency;
+
+    public float theta;
+    public Vector3 axis = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+        if (axis == Vector3.zero)
+        {
+            axis = Random.insideUnitSphere;
+            axis.y = 0;
+            axis.Normalize();
+        }
+        // Uncomment to use the job system
+        //SwayManager1.Instance.Add(this);
+    }
+	
+    // Comment out to use the job system
+    
+	void Update () {
+        transform.localRotation = Quaternion.AngleAxis(
+            Mathf.Sin(theta) * angle, axis);
+        theta += frequency * Time.deltaTime * Mathf.PI * 2.0f;
+	} 
+}
