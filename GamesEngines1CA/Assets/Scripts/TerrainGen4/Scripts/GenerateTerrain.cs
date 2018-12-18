1d6ef94c294c2bd2f2fd7b03a4f94f411542c144
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2018-12-18 20:49:11 +0000

    Finally got land gen working, next is trees

---
+++ GamesEngines1CA/Assets/Scripts/TerrainGen4/Scripts/GenerateTerrain.cs
@@ -0,0 +1,65 @@
+﻿/*******************************************************
+ * Copyright (C) 2015-2017 Penny de Byl (Holistic3d.com)
+ * 
+ * This file is part of InfiniteTerrainTrees Tutorial Project.
+ * 
+ *******************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateTerrain : MonoBehaviour {
+
+	int heightScale = 5;
+	float detailScale = 5.0f;
+	List<GameObject> myTrees = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		for(int v = 0; v < vertices.Length; v++)
+		{
+			vertices[v].y = Mathf.PerlinNoise((vertices[v].x + this.transform.position.x)/detailScale,
+				                              (vertices[v].z + this.transform.position.z)/detailScale)*heightScale;
+
+			if(vertices[v].y > 2.6 && Mathf.PerlinNoise((vertices[v].x+5)/10,(vertices[v].z+5)/10)*10 > 4.6)
+			{
+				GameObject newTree = TreePool.getTree();
+				if(newTree != null)
+				{
+					Vector3 treePos = new Vector3(vertices[v].x + this.transform.position.x,
+												  vertices[v].y - 1,
+												  vertices[v].z + this.transform.position.z);
+					newTree.transform.position = treePos;
+					newTree.SetActive(true);
+					myTrees.Add(newTree);
+
+				}
+			}
+
+		}
+
+		mesh.vertices = vertices;
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+		this.gameObject.AddComponent<MeshCollider>();
+
+	}
+
+	void OnDestroy()
+	{
+		for(int i = 0; i < myTrees.Count; i++)
+		{
+			if(myTrees[i] != null)
+				myTrees[i].SetActive(false);
+		}
+		myTrees.Clear();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GamesEngines1CA/Assets/Scripts/TerrainGen4/Scripts/TreePool.cs
@@ -0,0 +1,45 @@
+﻿/*******************************************************
+ * Copyright (C) 2015-2017 Penny de Byl (Holistic3d.com)
+ * 
+ * This file is part of InfiniteTerrainTrees Tutorial Project.
+ * 
+ *******************************************************/
+using UnityEngine;
+using System.Collections;
+
+public class TreePool : MonoBehaviour {
+
+	static int numTrees = 1000;
+	public GameObject treePrefab;
+	static GameObject[] trees;
+
+	// Use this for initialization
+	void Start () {
+	
+		trees = new GameObject[numTrees];
+		for(int i = 0; i < numTrees; i++)
+		{
+			trees[i] = (GameObject) Instantiate(treePrefab, Vector3.zero, Quaternion.identity);
+			trees[i].SetActive(false);
+		}
+
+	}
+	
+	
+	static public GameObject getTree()
+	{
+		for(int i = 0; i < numTrees; i++)
+		{
+			if(!trees[i].activeSelf)
+			{
+				return trees[i];
+			}
+		}
+		return null;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
