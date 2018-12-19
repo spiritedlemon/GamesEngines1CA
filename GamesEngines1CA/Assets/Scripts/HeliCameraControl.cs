715885555ee745fe93dfea62cdf7779cd9fbc454
Simon O'Leary <spiritedlemon@hotmail.co.uk>
2018-12-19 16:26:12 +0000

    In helicopter player can look up and down but not left or right

---
+++ GamesEngines1CA/Assets/Scripts/HeliCameraControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliCameraControl : MonoBehaviour {
+
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisLimit;
+
+    private void Awake()
+    {
+        LockCursor();
+		//Limit will be used to stop the player looking up/down more than 90'
+        xAxisLimit = 0.0f; 
+    }
+
+
+    private void LockCursor()
+    {
+		//locks cursor to centre of view (and invisible, press [escape] to make re-appear)
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xAxisLimit += mouseY;
+
+        if(xAxisLimit > 90.0f)
+        {
+            xAxisLimit = 90.0f;
+            mouseY = 0.0f;
+            LimitXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisLimit < -90.0f)
+        {
+            xAxisLimit = -90.0f;
+            mouseY = 0.0f;
+            LimitXAxisRotationToValue(90.0f);
+        }
+
+		//For turning the body and not just the camera:
+        transform.Rotate(Vector3.left * mouseY);
+        //playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void LimitXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
--- GamesEngines1CA/Assets/Scripts/PhysicsSpawner.cs
+++ GamesEngines1CA/Assets/Scripts/PhysicsSpawner.cs
@@ -3,57 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PhysicsSpawner : MonoBehaviour {
-    //public GameObject cubePrefab;
     public GameObject HeliPrefab;
     public GameObject player;
 
     public LayerMask groundLM;
-    // Use this for initialization
-	
-	/*
-    void CreateTower(float radius, int height, int segments, Vector3 point)
-    {
-        float thetaInc = (Mathf.PI * 2.0f) / (float)segments;
+    
 
-        for (int h = 0; h < height; h++)
-        {
-            for (int i = 0; i < segments; i++)
-            {
-                float theta = thetaInc * i + (h * thetaInc * 0.5f);
-                float x = radius * Mathf.Sin(theta);
-                float z = radius * Mathf.Cos(theta) + 20;
-                GameObject cube = GameObject.Instantiate<GameObject>(cubePrefab);
-                cube.transform.rotation = Quaternion.AngleAxis(theta * Mathf.Rad2Deg, Vector3.up);
-                cube.transform.position = point + new Vector3(x, h, z);
-                cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 0.8f);
-            }
-        }
-    }
-	*/
-
-    void CreateWorm(Vector3 point, Quaternion q)
+    void SpawnHeli(Vector3 point, Quaternion q)
     {
         GameObject heli = GameObject.Instantiate<GameObject>(HeliPrefab, point, q);
 
     }
-
-	void Start () {        
-    }
 	
 	// Update is called once per frame
-	void Update () {
-		/*
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            RaycastHit rch;
-            if (Physics.Raycast(player.transform.position, player.transform.forward, out rch, 100, groundLM))
-            {
-                Vector3 p = rch.point;
-                p.y = 0.5f;
-                CreateTower(3, 10, 12, p);
-            }
-        }
-		*/
+	void Update () 
+	{
 		
 		//On f press - Spawn helicopter
         if (Input.GetKeyDown(KeyCode.F))
@@ -66,7 +30,7 @@ public class PhysicsSpawner : MonoBehaviour {
                 Quaternion q = player.transform.rotation;
                 Vector3 xyz = q.eulerAngles;
                 q = Quaternion.Euler(0, xyz.y + 90, 0);
-                CreateWorm(p, q);
+                SpawnHeli(p, q);
             }
         }       
     }
